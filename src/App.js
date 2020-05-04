26a2180478cfe21faae72bd109c2fc366de0927c
Centurion256 <Vkuchynskyy@gmail.com>
2020-05-04 06:33:39 +0300

    Week 1 commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,62 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Body, Search} from './components';
+import {getGlobalData} from './API/API.js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      data: null //displays global data by default
+    }
+    // console.log(`In the constructor, ${this.state.data}, and ${this.state.data.cases}`);
+  }
+  
+  async componentDidMount() {
+
+    console.log("Awaited");
+    this.setState({
+      data: await getGlobalData()
+    });
+    console.log("The state has been set");
+  }
+
+  handleChangeCountry = async (info) => {
+
+    this.setState({
+      data: await info
+    });
+    console.log("The state has been reset");
+  }
+
+  render() {
+
+    if (this.state.data === null)
+    {
+      return null; //Promise not yet resolved, do not render.
+    }
+    return (
+      <div className="App">
+        <div style={{gridArea: 't'}}>
+          <h1>
+            Covid-19 Tracker
+          </h1>
+        </div>
+        <div style={{gridArea: 'a'}}>
+          <Search onCountryChange={this.handleChangeCountry}/>
+        </div>
+        <div style={{gridArea: 'c'}}>
+          <Body data={this.state.data}/>
+        </div>
+      </div>
+    );
+  }
+  // async componentDidMount() {
+
+  //   const data = await getCountryData('ukraine');
+  //   console.log(data);
+  // }
+};
 
 export default App;
